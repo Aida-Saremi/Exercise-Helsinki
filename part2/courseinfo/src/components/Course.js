767484a7a54b657fe338e932600ec219d317173e
Aida <aidasaremi@gmail.com>
2023-09-12 08:51:12 +0200

    This is not work,but I want to ask something

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,25 +1,41 @@
-import React from 'react'
-import Header from './Header'
-import Content from './Content'
+// import React from 'react'
+// import Header from './Header'
+// import Content from './Content'
+// import Total from './Total'
 
-const Course= (props) => {
-  console.log(props)
-    
+// const Course= (props) => {
+//   console.log(props.courses[0])
   
+//   return (
+//     <div>
+      
+//       <Header course={props.courses[0].name}/>
+//       <Content parts={props.courses[0].parts} />
+//       <Total parts={props.courses[0].parts}/>
+//       <Header course={props.courses[1].name}/>
+//       <Content parts={props.courses[1].parts} />
+//       <Total parts={props.courses[1].parts}/>
+
+//     </div>
+//   )
+//   }
+  
+//   export default Course
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
+
+const Course = (props) => {
   return (
     <div>
+     
+          <Header name={props.name}/>
+            <Content parts={props.parts}/>
+            <Total parts={props.parts}/>
+        </div>
       
-      <Header course={props.course.name}/>
-      <Content parts={props.course.parts} />
-    
+  );
+};
 
-    </div>
-  )
-  }
-  
-  export default Course
-
-
-
-
-
+export default Course;
--- part2/courseinfo/src/components/Header.js
+++ part2/courseinfo/src/components/Header.js
@@ -5,7 +5,7 @@ const Header= (props) => {
   
     return (
       <div>
-        <h1>{props.course}</h1>
+        <h1>{props}</h1>
        
       </div>
     )
