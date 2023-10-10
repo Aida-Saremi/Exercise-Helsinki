26e454a24470369bf3338da3c19f21a80537500a
Aida <aidasaremi@gmail.com>
2023-10-10 14:27:58 +0200

    2.18(step2-without button search)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,43 +1,130 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// const App = () => {
+//   const [value, setValue] = useState('');
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const [country, setCountry] = useState({});
+//   const [error, setError] = useState('');
+
+//   useEffect(() => {
+//     if (searchQuery) {
+//       axios
+//         .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${searchQuery}`)
+//         .then(response => {
+//           const data = response.data;
+//           if (data.length > 10) {
+//             setError('Too many countries, please make your query more specific.');
+//             setCountry([]);
+//           } else {
+//             setError('');
+//             setCountry(data);
+//           }
+//         })
+//         .catch(error => {
+//           console.error('Error fetching data:', error);
+//           setError('An error occurred while fetching data.');
+//           setCountry([]);
+//         });
+//     }
+//   }, [searchQuery]);
+//   const handleChange = (event) => {
+//     setValue(event.target.value);
+//   }
+
+//   const onSearch = (event) => {
+//     event.preventDefault();
+//     setSearchQuery(value);
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={onSearch}>
+//         Find Countries: <input value={value} onChange={handleChange} />
+//         <button type="submit">Search</button>
+//       </form>
+
+//       {country.name && (
+//         <div>
+//           <h1>{country.name.common}</h1>
+//           <p>Capital {country.capital}</p>
+//           <p>Area {country.area}</p>
+
+//           {country.languages && (
+//             <div>
+//               <h3>Languages:</h3>
+//               <ul>
+//                 {Object.entries(country.languages).map(([code, name]) => (
+//                   <li key={code}>{name}</li>
+//                 ))}
+//               </ul>
+//             </div>
+//           )}
+
+
+
+//           {country.flags?.png && (
+//             <img
+//               style={{ margin: "20px 0" }}
+//               width="100px"
+//               src={country.flags.png}
+//               alt={`${country.name.common} flag`}
+//             />
+//           //   {/* If country.flags is defined, 
+//           //  it checks if country.flags.png is also defined. If it is,
+//           //  the expression evaluates to true; otherwise, it evaluates to false.
+
+//           //      &&: This is the logical AND operator. If the condition before it is true,
+//           //    it proceeds to the next part. If the condition is false,
+//           //     it short-circuits, and the subsequent part is not executed. */}
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
   const [value, setValue] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
   const [country, setCountry] = useState({});
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    if (searchQuery) {
+    if (value) {
       axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${searchQuery}`)
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${value}`)
         .then(response => {
           const data = response.data;
-          setCountry(data);
-          console.log(data);
-          console.log(Array.isArray(country.languages));
+          if (data.length > 10) {
+            setError('Too many countries, please make your query more specific.');
+            setCountry([]);
+          } else {
+            setError('');
+            setCountry(data);
+          }
         })
         .catch(error => {
           console.error('Error fetching data:', error);
-          // Handle errors as needed
+          setError('An error occurred while fetching data.');
+          setCountry([]);
         });
     }
-  }, [searchQuery]);
+  }, [value]);
 
   const handleChange = (event) => {
     setValue(event.target.value);
   }
 
-  const onSearch = (event) => {
-    event.preventDefault();
-    setSearchQuery(value);
-  }
-
   return (
     <div>
-      <form onSubmit={onSearch}>
-        Find Countries: <input value={value} onChange={handleChange} />
-        <button type="submit">Search</button>
-      </form>
+      <label >Find Countries:</label>
+      <input id="search" value={value} onChange={handleChange} />
 
       {country.name && (
         <div>
@@ -56,8 +143,6 @@ const App = () => {
             </div>
           )}
 
-
-
           {country.flags?.png && (
             <img
               style={{ margin: "20px 0" }}
@@ -65,16 +150,11 @@ const App = () => {
               src={country.flags.png}
               alt={`${country.name.common} flag`}
             />
-          //   {/* If country.flags is defined, 
-          //  it checks if country.flags.png is also defined. If it is,
-          //  the expression evaluates to true; otherwise, it evaluates to false.
-
-          //      &&: This is the logical AND operator. If the condition before it is true,
-          //    it proceeds to the next part. If the condition is false,
-          //     it short-circuits, and the subsequent part is not executed. */}
           )}
         </div>
       )}
+
+      {error && <p>{error}</p>}
     </div>
   );
 };
