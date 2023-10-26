ef2026d4e0bf4477a50a52b65350367c7a0e8f6b
Aida <aidasaremi@gmail.com>
2023-10-26 22:48:03 +0200

    3.3 Phonebook backend step3

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -5,16 +5,26 @@ app.use(express.json())
 
 let persons = [
     
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
     ]
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -35,6 +45,18 @@ app.get('/info', (request, response) => {
 
   response.send(infoMessage);
 });
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).send('Not Found');
+  }
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
